8deacb6aa2905f8d60dcde533e0b407c8f60ae5c
380758137 <380758137@qq.com>
2017-09-01 17:56:57 +0800

    周五

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    
-    <router-view></router-view>
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    leftNav
+  }
 }
 </script>
 
@@ -18,6 +24,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.main{
+  float: left;
+  width: 95%;
+  background-color: gainsboro;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
